fb61e24ae89a00c07cf4b851bfec5a5a422b3df6
PPriyadarshi5498 <167844572+PPriyadarshi5498@users.noreply.github.com>
2025-04-14 21:55:24 +0530

    projected completed

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,7 +7,7 @@ import ChatLabel from './ChatLabel'; // Add this line to import ChatLabel
 
 const Sidebar = ({ expand, setExpand }) => {
   const { openSignIn } = useClerk();
-  const { user } = useAppContext();
+  const { user, chats, createNewChat } = useAppContext();
   const [openMenu, setOpenMenu] = useState({id: 0, open: false})
 
   return (
@@ -47,7 +47,7 @@ const Sidebar = ({ expand, setExpand }) => {
         </div>
 
         {/* New Chat */}
-        <button className={`mt-8 flex items-center justify-center cursor-pointer ${
+        <button onClick={createNewChat} className={`mt-8 flex items-center justify-center cursor-pointer ${
           expand
             ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max"
             : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"
@@ -67,7 +67,8 @@ const Sidebar = ({ expand, setExpand }) => {
         {/* Recents */}
         <div className={`mt-8 text-white/25 text-sm ${expand ? "block" : "hidden"}`}>
           <p className="my-1">Recents</p>
-          <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+          {chats.map((chat, index)=><ChatLabel key={index} name={chat.name} id={chat_id} openMenu={openMenu} setOpenMenu={setOpenMenu}/>)}
+          
         </div>
       </div>
 
